9bfa69a41f4e4795a4239039fb05a259fadc112f
iratxe <iratxe.aviles@ikasle.egibide.org>
2024-02-05 20:49:13 +0100

    Proyecto creado

---
+++ Ejercicio_OnlineShop/Ejercicio_OnlineShop/Ejercicio_OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ejercicio_OnlineShopApp.swift
+//  Ejercicio_OnlineShop
+//
+//  Created by  on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Ejercicio_OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
