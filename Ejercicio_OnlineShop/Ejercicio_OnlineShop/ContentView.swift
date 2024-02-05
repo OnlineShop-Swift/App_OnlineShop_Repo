ac555c3c51ea4dcf603b354056367ec35a4eab16
irene.sarasua <irene.sarasua@ikasle.egibide.org>
2024-02-05 21:08:06 +0100

    Prueba 2

--- Ejercicio_OnlineShop/Ejercicio_OnlineShop/ContentView.swift
+++ Ejercicio_OnlineShop/Ejercicio_OnlineShop/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, Iratxe!")
+            Text("Hello, Irene!")
         }
         .padding()
     }
