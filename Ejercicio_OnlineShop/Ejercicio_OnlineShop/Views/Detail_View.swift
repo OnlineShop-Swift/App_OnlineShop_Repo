87a42ab0540fa87a9450d461cb2d8b58cbc07ad1
irene.sarasua <irene.sarasua@ikasle.egibide.org>
2024-02-23 21:59:30 +0100

    empezando

--- Ejercicio_OnlineShop/Ejercicio_OnlineShop/Views/Detail_View.swift
+++ Ejercicio_OnlineShop/Ejercicio_OnlineShop/Views/Detail_View.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct Detail_View: View {
+    var producto: Producto
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            AsyncImage(url: URL(string: producto.image)){ image in
+                image.resizable()
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: 150, height: 150)
+            .padding(.bottom)
+            Text(producto.title)
+                .font(.title)
+                .padding(.vertical)
+                .fontWeight(.bold)
+            Text(producto.description)
+                .font(.title)
+                .padding(.vertical)
+                .fontWeight(.bold)
+            
+           
+            
+        }
+        
+        
+        
     }
 }
+struct BotonAddCart : View {
+    
+    var body: some View {
+        Button("Change day time", action: addCart)
+            .padding()
+            .padding(.horizontal)
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(10.0)
+            .fontWeight(.bold)
+        Spacer()
+    }
+    
+    func addCart() {
+        print("bh")
+            
+    }
+    
+}
+
+
 
 #Preview {
-    Detail_View()
+    Detail_View(producto: Producto(id: 1, title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops", price: 109.95, description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday", image: "https://fakestoreapi.com…PKd-2AYL._AC_SL1500_.jpg", rating: Rating(rate: 3.9)))
 }
