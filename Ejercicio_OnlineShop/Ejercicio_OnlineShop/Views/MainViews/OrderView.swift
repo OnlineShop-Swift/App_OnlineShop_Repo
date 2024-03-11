7b96d772a4500aa1fed1ac8a8d6f3fe0a5dab192
iratxe <iratxe.aviles@ikasle.egibide.org>
2024-03-11 19:54:06 +0100

    Pa la irene

---
+++ Ejercicio_OnlineShop/Ejercicio_OnlineShop/Views/MainViews/OrderView.swift
@@ -0,0 +1,60 @@
+//
+//  OrderView.swift
+//  Ejercicio_OnlineShop
+//
+//  Created by  on 11/3/24.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    @EnvironmentObject var opciones: Opciones
+    
+    var body: some View {
+        VStack{
+            NavigationSplitView{
+                List(opciones.seleccion){ current in
+                            ProductoRow(producto: current)
+                    }
+                    .navigationTitle("Shopping Cart")
+                
+                }detail: {
+                }
+            Spacer()
+           
+            BotonAddOrder()
+                .environmentObject(opciones)
+        }
+        
+    }
+    
+}
+
+struct BotonAddOrder : View {
+    @ObservedObject private var vm = ProductosViewModel()
+    @EnvironmentObject var opciones: Opciones
+    var body: some View {
+        Button("$" + precioTotal() + " - Checkout", action: addOrder)
+            .padding()
+            .padding(.horizontal)
+            .foregroundColor(.white)
+            .background(Color.coloPrim)
+            .cornerRadius(10.0)
+            .fontWeight(.bold)
+        
+    }
+    
+    func addOrder() {
+        
+    }
+    func precioTotal() -> String{
+        var total = 0.0
+        for elemento in opciones.seleccion {
+            total += elemento.price
+        }
+        return String(total)
+    }
+    
+}
+
+
