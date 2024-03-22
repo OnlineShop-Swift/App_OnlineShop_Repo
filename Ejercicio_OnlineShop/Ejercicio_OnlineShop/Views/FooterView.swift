370ce511bec8e685b92c6d7615352fe1605a56c9
irene.sarasua <irene.sarasua@ikasle.egibide.org>
2024-03-22 21:24:15 +0100

    pruebas zstack

--- Ejercicio_OnlineShop/Ejercicio_OnlineShop/Views/FooterView.swift
+++ Ejercicio_OnlineShop/Ejercicio_OnlineShop/Views/FooterView.swift
@@ -16,7 +16,7 @@ struct FooterView: View {
                 .environmentObject(opciones)                .tabItem {
                     Label("Home", systemImage: "house.fill")
                 }
-            AccountView()
+            AccountView(list: ProductosViewModel(), producto: Producto(id: 1, title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops", price: 109.95, description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday", image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg", rating: Rating(rate: 3.9)))
                 
                 .tabItem {
                     Label("Account", systemImage: "person.fill")
--- Ejercicio_OnlineShop/Ejercicio_OnlineShop/Views/MainViews/AccountView.swift
+++ Ejercicio_OnlineShop/Ejercicio_OnlineShop/Views/MainViews/AccountView.swift
@@ -8,11 +8,105 @@
 import SwiftUI
 
 struct AccountView: View {
-    var body: some View {
-        Text("Hello, Account!")
-    }
-}
+    @ObservedObject var list: ProductosViewModel
+    @EnvironmentObject var opciones: Opciones
+    @State var producto: Producto
 
-#Preview {
-    AccountView()
-}
+    /*
+    @State var nombre: String = ""
+    //@AppStorage("apellido") var apellido: String = ""
+    //@AppStorage("email") var email: String = ""
+    //@AppStorage("birthday") var birthday: String = ""
+    //@AppStorage("vip") var vip: Bool = false
+    //@AppStorage("ofertas") var ofertas: Bool = false
+    
+    init() {
+        let dato = UserDefaults.standard.string(forKey: "nombre")
+        _nombre = State(wrappedValue: dato ?? "")
+    }
+     */
+    var body: some View {
+        
+        ZStack{
+            NavigationSplitView{
+                    List(list.productos){ current in
+                        NavigationLink{
+                            
+                        } label: {
+                            ProductoRow(producto: current)
+                        }
+                    }
+                    .navigationTitle("Products")
+                
+                }detail: {
+                    Text("Select Product")
+                }
+                .blur(radius: 3.5)
+            VStack {
+                AsyncImage(url: URL(string: producto.image)){ image in
+                    image.resizable()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: 150, height: 150)
+                .padding(.bottom)
+                
+                Text(producto.title)
+                    .font(.title)
+                    .padding(.vertical)
+                    .fontWeight(.bold)
+                Text(producto.description)
+                    .font(.title)
+                    .padding(.vertical)
+                    .lineLimit(3)
+                RatingView(rating: Decimal(producto.rating.rate))
+                    .padding(.bottom)
+                BotonAddCart(producto: $producto)
+                    .environmentObject(opciones)
+            
+                
+            }
+            .padding()
+            .background(.white)
+            .border(.blue)
+            
+            
+        }
+        
+        /*
+        VStack{
+            Text("Account")
+                .bold()
+                .font(.title)
+                .padding()
+            Form{
+                Section(header: Text("PERSONAL INFO")) {
+                    TextField("First name", text: $nombre)
+                                
+                    //TextField("Last name", text: $apellido)
+                                
+                    //TextField("Email", text: $email)
+                    
+                    Button("Save changes", action: guardarCambios)
+                }
+                Section(header: Text("SETTINGS")) {
+                    
+                }
+                .onAppear{
+                    nombre = UserDefaults.standard.string(forKey: "nombre") ?? ""
+                }
+            }
+         */
+        }
+         
+        
+    }
+     /*
+    func guardarCambios(){
+        UserDefaults.standard.set(nombre, forKey: "nombre")
+        //self.presentationMode.wrappedValue.dismiss()
+        
+    }
+      */
+     
+
